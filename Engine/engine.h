12342965ad98462aab939140f8d684ebbcf393d8
Silver-Talen <silver_talen@hotmail.com>
2018-06-25 14:50:37 -0600

    Add project files.

---
+++ Engine/engine.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+
+#ifdef ENGINE_EXPORTS  
+#define ENGINE_API __declspec(dllexport)   
+#else  
+#define ENGINE_API __declspec(dllimport)   
+#endif 
+
+class ENGINE_API Engine
+{
+public:
+	Engine() {}
+	~Engine() {}
+
+	void Display()
+	{
+		std::cout << "Hello World !" << std::endl;
+	}
+};
