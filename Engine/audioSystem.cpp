0c475c5c11517d94c51c2c1a23f7dfb74782df20
Silver-Talen <silver_talen@hotmail.com>
2018-07-12 14:19:19 -0600

    7-12

--- Engine/audioSystem.cpp
+++ Engine/audioSystem.cpp
@@ -1,13 +1,42 @@
 #include "audioSystem.h"
+#include <assert.h>
 
 bool AudioSystem::Initialize(Engine * engine)
 {
 	m_engine = engine;
 
+	FMOD_RESULT result = FMOD::System_Create(&m_fmodSystem);
+	assert(result == FMOD_OK);
+
+	void* extradriverdata = nullptr;
+	result = m_fmodSystem->init(32, FMOD_INIT_NORMAL, extradriverdata);
+	assert(result == FMOD_OK);
+
 	return true;
 }
 
 void AudioSystem::Shutdown()
+{
+	for (auto sound : m_sounds)
+	{
+		 sound.second->release();
+	}
+	m_sounds.clear();
+	m_fmodSystem->close();
+	m_fmodSystem->release();
+}
+
+void AudioSystem::AddSound(const std::string & id, const std::string & filename)
+{
+
+}
+
+void AudioSystem::PlaySound(const std::string & id)
+{
+
+}
+
+void AudioSystem::RemoveSound(const std::string & id)
 {
 
 }
--- Engine/audioSystem.h
+++ Engine/audioSystem.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "singleton.h"
 #include "engine.h"
+#include <string>
+#include <map>
+#include <fmod.hpp>
 
 class AudioSystem : public Singleton<AudioSystem>
 {
@@ -9,10 +12,16 @@ public:
 	bool Initialize(Engine* engine);
 	void Shutdown();
 
+	void AddSound(const std::string& id, const std::string& filename); 
+	void PlaySound(const std::string& id); 
+	void RemoveSound(const std::string& id);
+
 public:
 	AudioSystem() {}
 	~AudioSystem() {}
 
 private:
 	Engine * m_engine;
+	FMOD::System* m_fmodSystem;
+	std::map<std::string, FMOD::Sound*> m_sounds;
 };
